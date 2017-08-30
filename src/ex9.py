9fd9ad845aa8c036dfde750eee4763fb8f22b003
mark <gao2524289@outlook.com>
2017-08-30 23:12:18 +0800

    add some exmaple

---
+++ src/ex9.py
@@ -0,0 +1,16 @@
+"I am 6'2\" tall." #将字符串中的双引号转义
+tabby_cat="\tI'm tabbrd in."
+persian_cat ="I'm split\non a line."
+backslash_cat="I'm \\a \\ cat"
+
+fat_cat="""\
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t Grass
+"""
+
+print (tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
